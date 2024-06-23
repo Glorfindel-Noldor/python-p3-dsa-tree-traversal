9f423841b92ab14d375d97c43174938fc50bf197
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-06-23 15:55:56 -0500

    brut force looking

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
+
 class Tree:
   def __init__(self, root = None):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+
+    grabbed = [self.root]
+
+    while grabbed:
+      currently_grabbing = grabbed.pop()
+      if currently_grabbing.get('id') == id:
+          return currently_grabbing
+      
+      grabbed.extend(currently_grabbing.get('children', [] )) 
+    
+    return None
